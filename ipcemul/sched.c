167bf8910dad7abbc1cce8298c6dcb16e5387a97
mkevac <mkevac>
2005-09-28 18:54:42 +0000

    Dirty implementation of using real time, instead of virtual (with incrementing variable)

--- ipcemul/sched.c
+++ ipcemul/sched.c
@@ -18,44 +18,57 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 #include "fork.h"
 #include <unistd.h>
 #include "time.h"
+#include <time.h>
 
 extern struct task *root_task;
 extern struct task *current_proc;
-extern int *timeptr;
+//extern int *timeptr;
 
 int scheduler(void)
 {
-	struct task *tsk_add_time = root_task;
-	struct task *tsk;
-	int begin;
-	
-	tsk = Find_max_prio();
-	
-	current_proc = tsk;
-	
-	usleep(300);
-	printf("\nbegin work task with pid %d at time %d\n",tsk->pid, *timeptr);
-	begin=*timeptr;
-	if(tsk->code == NULL)
-	{
-		printf("NULL proc\n");
-	}
-	else
-	{
-		ExecCode(tsk);
-	}
-	
-	while(tsk_add_time != NULL)
-	{
-		tsk_add_time->run_time+=1;
-		tsk_add_time = tsk_add_time->next;
-	}
-	
-	printf("end work task with pid %d at time %d\n",tsk->pid, *timeptr);
-	printf("time in work is %d\n\n", *timeptr-begin);
-	
-	return 0;
+    struct timespec *timevalue;
+    struct task *tsk_add_time = root_task;
+    struct task *tsk;
+    long begin;
+
+    timevalue = (struct timespec *)malloc(sizeof(struct timespec));
+
+    tsk = Find_max_prio();
+
+    current_proc = tsk;
+
+    usleep(300);
+    
+    if (clock_gettime(CLOCK_REALTIME, timevalue)!=0)
+        printf("Error getting time\n");
+    
+    begin=((int)(timevalue->tv_sec))*1000000000.0+((int)(timevalue->tv_nsec));
+    printf("\nbegin work task with pid %d at time %e\n",tsk->pid, (double)begin);
+
+    if(tsk->code == NULL)
+    {
+        printf("NULL proc\n");
+    }
+    else
+    {
+        ExecCode(tsk);
+    }
+
+    while(tsk_add_time != NULL)
+    {
+        tsk_add_time->run_time+=1;
+        tsk_add_time = tsk_add_time->next;
+    }
+
+    if (clock_gettime(CLOCK_REALTIME, timevalue)!=0)
+                printf("Error getting time\n");
+    
+    printf("end work task with pid %d at time %e\n",tsk->pid, ((int)(timevalue->tv_sec))*1000000000.0+((int)(timevalue->tv_nsec)));
+    printf("time in work is %e\n\n",((int)(timevalue->tv_sec))*1000000000.0+((int)(timevalue->tv_nsec))-begin);
+
+    return 0;
 }
