e83f845136e0f20ea3825af905d276de1842bda8
mkevac <mkevac>
2005-10-25 19:13:47 +0000

    null

--- ipcemul/generator.c
+++ ipcemul/generator.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"msg.h"
-#include"fork.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "msg.h"
+#include "fork.h"
 
 extern struct msg_msg *root_msg_msg;
 extern struct process *current_proc;
